9e6b8e12de1d1d67ee6bbaed7131219efe194ffa
Andrgoit <stakgrom@gmail.com>
2023-10-12 20:34:42 +0300

    update readme

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { fetchWeather } from "./redux/weather/weatherOperations";
 import { SelectLocation } from "./redux/weather/weatherSelectors";
 import { SelectQuery } from "./redux/query/querySelectors";
 
-import { ImageBg, Header, Current, Forecast } from "./components";
+import { ImageBg, Header, Current, Forecast, Banner } from "./components";
 
 function App() {
   const city = useSelector(SelectQuery);
@@ -21,11 +21,13 @@ function App() {
   return (
     <ImageBg>
       <Header />
-      {location && (
+      {location ? (
         <div className="flex flex-col gap-6 items-center">
           <Current />
           <Forecast />
         </div>
+      ) : (
+        <Banner />
       )}
     </ImageBg>
   );
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,29 @@
 import { BiSolidQuoteAltLeft, BiSolidQuoteAltRight } from "react-icons/bi";
 
+import { motion } from "framer-motion";
+
+const animation = {
+  hidden: {
+    x: 100,
+    opacity: 0,
+  },
+  visible: (custom) => ({
+    x: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.2 },
+  }),
+};
+
 export default function Banner() {
   return (
-    <div className=" hidden sm:flex rounded-lg flex-col items-end gap-5 absolute top-1/2 right-0 backdrop-blur-sm bg-white/10 font-handfree  text-white py-6 px-7">
+    <motion.div
+      initial="hidden"
+      // whileInView="visible" при загрузке экрана
+      animate="visible"
+      variants={animation}
+      custom={3}
+      className=" hidden sm:flex rounded-lg flex-col items-end gap-5 /2 right-0 backdrop-blur-sm bg-black/40 font-handfree  text-white py-6 px-7"
+    >
       <div className="flex gap-1 text-2xl md:text-6xl">
         {" "}
         <BiSolidQuoteAltLeft size={32} />
@@ -10,6 +31,6 @@ export default function Banner() {
         <BiSolidQuoteAltRight size={32} />
       </div>
       <p className=" text-lg md:text-2xl">William Shakespeare</p>
-    </div>
+    </motion.div>
   );
 }
